10d9a8eff06b913f071842316f6c6451a2cb9257
Muhsin <muhsin7ak@gmail.com>
2019-07-12 21:40:40 +0530

    Updates

---
+++ precache-manifest.4e7a18dd68efef3370e87effcdcc3e01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14007ff0470cf85d3c3d05e3daab5e99",
+    "url": "/download-speed-calculator/index.html"
+  },
+  {
+    "revision": "66879e48b05953a34142",
+    "url": "/download-speed-calculator/static/css/main.5b7a8159.chunk.css"
+  },
+  {
+    "revision": "dff8f5e6896d4e47fb65",
+    "url": "/download-speed-calculator/static/js/2.d16ba94f.chunk.js"
+  },
+  {
+    "revision": "66879e48b05953a34142",
+    "url": "/download-speed-calculator/static/js/main.dd53d55e.chunk.js"
+  },
+  {
+    "revision": "81e4da498566f68fc807",
+    "url": "/download-speed-calculator/static/js/runtime~main.2d5f15c9.js"
+  }
+]);
